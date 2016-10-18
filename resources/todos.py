e0f063f70253a41c5669f69003ec5be3bee500db
Chris H <crhowell1@gmail.com>
2016-10-18 15:51:09 -0700

    Add put and delete methods
    Add Todo resource

--- resources/todos.py
+++ resources/todos.py
@@ -1,8 +1,9 @@
 import datetime
+import json
 
-from flask import jsonify, Blueprint, g
+from flask import jsonify, Blueprint, g, abort, make_response
 
-from flask_restful import (Resource, Api, reqparse,
+from flask_restful import (Resource, Api, reqparse, url_for,
                            inputs, fields, marshal, marshal_with)
 
 from auth import auth
@@ -17,6 +18,15 @@ todo_fields = {
 }
 
 
+def todo_or_404(todo_id):
+    try:
+        todo = models.Todo.get(models.Todo.id==todo_id)
+    except models.Todo.DoesNotExist:
+        abort(404)
+    else:
+        return todo
+
+
 class TodoList(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
@@ -24,7 +34,7 @@ class TodoList(Resource):
             'name',
             required=True,
             help='No name provided',
-            location=['form', 'jsofn']
+            location=['form', 'json']
         )
         self.reqparse.add_argument(
             'description',
@@ -39,6 +49,7 @@ class TodoList(Resource):
                  for todo in models.Todo.select()]
         return {'todos': todos}
 
+    @marshal_with(todo_fields)
     @auth.login_required
     def post(self):
         args = self.reqparse.parse_args()
@@ -48,9 +59,69 @@ class TodoList(Resource):
             user=g.user,
             **args
         )
-        return todo
+        return (todo, 201, {
+            'Location': url_for('resources.todos.todo', id=todo.id)
+        })
+
+
+class Todo(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            required=True,
+            help='No todo name provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'description',
+            required=True,
+            help='No todo description provided',
+            location=['form', 'json']
+        )
+        super().__init__()
+
+    @marshal_with(todo_fields)
+    def get(self, id):
+        return todo_or_404(id)
+
+    @marshal_with(todo_fields)
+    @auth.login_required
+    def put(self, id):
+        args = self.reqparse.parse_args()
+        try:
+            todo = models.Todo.select().where(
+                models.Todo.user==g.user,
+                models.Todo.id==id
+            ).get()
+        except models.Todo.DoesNotExist:
+            return make_response(json.dumps(
+                {'error': 'That review does not exist or is not editable'}
+            ), 403)
+        query = todo.update(**args)
+        query.execute()
+        todo = todo_or_404(id)
+        return (todo, 200, {
+            'Location': url_for('resources.todos.todo', id=id)
+        })
+
+    @auth.login_required
+    def delete(self, id):
+        try:
+            todo = models.Todo.select().where(
+                models.Todo.user==g.user,
+                models.Todo.id==id
+            ).get()
+        except models.Todo.DoesNotExist:
+            return make_response(json.dumps(
+                {'error': 'That TODO  does not exist or is not editable'}
+            ), 403)
+        query = todo.delete()
+        query.execute()
+        return '', 204, {'Location': url_for('resources.todos.todos')}
 
 
 todos_api = Blueprint('resources.todos', __name__)
 api = Api(todos_api)
 api.add_resource(TodoList, '/todos', endpoint='todos')
+api.add_resource(Todo, '/todos/<int:id>', endpoint='todo')
